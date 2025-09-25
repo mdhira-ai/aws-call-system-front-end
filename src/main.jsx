77954e209b8f1d29de3aabcb0ffab4e9ca30c6e7
MD HABIBOR RAHMAN HIRA <68804057+mdhira-ai@users.noreply.github.com>
2025-09-25 08:51:39 -0400

    fix: update import path for routes in main.jsx and add vercel configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
 import DataTransferTest from "./peer-data-transfer-test/page";
 import App from "./App";
